7dc59894c43f180c2c508c539a3c614253ca08ed
Issac Moreno <issaxavier909@gmail.com>
2022-02-05 11:05:18 -0800

    CTRL + F and type replace, replace everything that is highlighted

--- src/config.js
+++ src/config.js
@@ -5,28 +5,32 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
-const baseUri = "ipfs://NewUriToReplace";
+const namePrefix = "*REPLACE WITH COLLECTION NAME*";
+const description = "*REPLACE WITH DESCRIPTION FOR COLLECTION*";
+const baseUri = "ipfs://*REPLACE WITH cID TO METADATA FROM IPFS*";
+// NOTE: After everything is replaced, type "node utils/updateBaseUri.js" in the terminal and push enter
+// Once the URI is updated, now enter "node index.js" in the terminal and it will generate a json folder we can upload to IPFS as well for our smart contract
 
-const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
-  external_url: "https://www.youtube.com/c/hashlipsnft",
-  creators: [
-    {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
-      share: 100,
-    },
-  ],
-};
+// UNCOMMENT ONLY IF WORKING WITH SOLANA NFTs
+// const solanaMetadata = {
+//   symbol: "YC",
+//   seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+//   external_url: "https://www.youtube.com/c/hashlipsnft",
+//   creators: [
+//     {
+//       address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+//       share: 100,
+//     },
+//   ],
+// };
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
+  // NOTE: Using growEditionSizeTo, we can create special folders for attributes to create a special set of 1/1, 2/2, 3/3, etc. that we use to divide our collection into seperate drops, auction or promote
   {
     growEditionSizeTo: 5,
     layersOrder: [
-      { name: "Background" },
+      { name: "Background" }, // REPLACE THESE EXAMPLE ATTRIBUTES (in layers folder)
       { name: "Eyeball" },
       { name: "Eye color" },
       { name: "Iris" },
@@ -34,9 +38,26 @@ const layerConfigurations = [
       { name: "Bottom lid" },
       { name: "Top lid" },
     ],
-  },
+  },  
+
+  // {
+  //   growEditionSizeTo: 10000,
+  //   layersOrder: [
+  //     { name: "Background" },
+  //     { name: "Eyeball" },
+  //     { name: "Eye color" },
+  //     { name: "Iris" },
+  //     { name: "Shine" },
+  //     { name: "Bottom lid" },
+  //     { name: "Top lid" },
+  //   ],
+  // },
 ];
 
+// NOTE: If we do end up adding a special set to the collection, we can make sure they are not minted in chronological/predictable way by changing this value to true
+// For example, if we want to seperate the drops into a Genesis collection and then mint cops and then robbers, we can leave this false
+// If you want the cops and robbers to be minted at random, you would need to set this to true, but then you can no longer have the Genesis collection because it would mix them in with the cops and robbers as well
+// If we come across a specific case where we would need to incorporate both functionalities, I will look into it, but this is the only limit so far
 const shuffleLayerConfigurations = false;
 
 const debugLogs = false;
